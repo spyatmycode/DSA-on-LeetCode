e0843dc31e92f129a04fae130b643eff08be32d4
Akeju Oluwanifemi <akejunifemi11@gmail.com>
2024-10-28 11:56:13 +0100

    Time: 41 ms (22.01%), Space: 16.5 MB (54.34%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -2,7 +2,8 @@ class Solution:
     def reverse(self, x: int) -> int:
 
 
-        print(-2147483651 < pow(-2,31)) 
+        #note to self: you're supposed to use the reversed value to check the conditions not x value
+
 
         result = (-abs(int(f"{abs(x)}"[::-1])) if f"{x}"[0] == "-" else int(f"{abs(x)}"[::-1]))
 
