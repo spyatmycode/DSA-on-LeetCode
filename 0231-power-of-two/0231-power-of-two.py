1622a2f26e47d5783873e75b09157dede76242f1
Akeju Oluwanifemi <akejunifemi11@gmail.com>
2024-11-03 16:09:37 +0100

    Time: 3 ms (15.63%), Space: 16.7 MB (13.7%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -3,12 +3,14 @@ class Solution:
 
         #Base case is the integer being one
         def solution(n):
-
+            #The base case
             if n == 1:
+                #This is 
                 return True
+            #The number is definitely not a power of two
             if n < 1:
                 return False
-        
+            #Call solution function recursively with the integer/2
             return solution(n/2)
     
         return solution(n)
