9ec0ad570f4c00fe7d24b39ee222d350c263cd5c
Akeju Oluwanifemi <akejunifemi11@gmail.com>
2024-11-18 04:43:47 +0100

    Time: 554 ms (71.22%), Space: 19.7 MB (80%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,35 +5,31 @@ class Solution:
 
         result = []
 
-
         for i, num in enumerate(nums):
 
-            if i > 0 and nums[i - 1] == num:
+            if i > 0 and num == nums[i - 1]:
                 continue
 
-            left, right = i + 1, len(nums) - 1
+            left, right = i+1, len(nums) - 1
 
             while left < right:
-                threeSum = num + nums[right] + nums[left]
+                threeSum = num + nums[left] + nums[right]
 
                 if threeSum < 0:
-                    left += 1
-                
+                    left +=1
+
                 elif threeSum > 0:
-                    right -= 1
+                    right -=1
 
                 else:
 
-                    result.append([num, nums[right], nums[left]])
+                    result.append([num, nums[left], nums[right]])
 
-                    left += 1
+                    left +=1
 
                     while left < right and nums[left] == nums[left - 1]:
                         left += 1
-                    
+
         
-
         return result
-
-                    
         
